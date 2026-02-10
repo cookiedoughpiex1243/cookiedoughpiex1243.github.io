86875bfcf99740444b85ff581c0df5112d53edb2
cookiedoughpiex1243 <joshxgaming1243@gmail.com>
2026-02-10 21:24:11 +0530

    Update logobox.js

--- resources/logobox.js
+++ resources/logobox.js
@@ -1,7 +1,7 @@
 function injectOverlay() {
   OverlayHTML = ` 
     <div id="myBox" class="boxoverlay">
-    <a href="login" class="download-btn" style="top: 20px; left: 20px;">Login</a>
+    <a href="login" class="download-btn" style="top: 20px; left: 20px; position: fixed;">Login</a>
     <div class="box">
       <span class="closebtn" onclick="closeBox()">&times;</span>
       <h2 class="glow-cyan"> Extra Info</h2>
