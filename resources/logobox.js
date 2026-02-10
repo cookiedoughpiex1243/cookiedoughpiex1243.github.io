cf767aeb829a039e3609bce57a746725046a6144
cookiedoughpiex1243 <joshxgaming1243@gmail.com>
2026-02-10 17:09:45 +0530

    Update logobox.js

--- resources/logobox.js
+++ resources/logobox.js
@@ -1,3 +1,17 @@
+function injectOverlay() {
+  OverlayHTML = '<div id="myBox" class="boxoverlay">
+    <div class="box">
+      <span class="closebtn" onclick="closeBox()">&times;</span>
+      <h2 class="glow-cyan"> Extra Info</h2>
+      <p> YIPPEE you clicked the cookie!</p>
+      <button class="download-btn" onclick="closeBox()"> Cool, im done</button>
+    </div>
+  </div>';
+document.body.insertAdjacentHTML('beforeend', OverlayHTML)
+}
+
+  injectOverlay();
+
 const boxoverlay = document.getElementById("myBox");
 function openBox() {
   boxoverlay.style.display = "block";
