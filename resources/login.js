09943cda400713570900947dd4a006f2de358b2c
cookiedoughpiex1243 <joshxgaming1243@gmail.com>
2026-02-10 22:09:02 +0530

    Update login.js

--- resources/login.js
+++ resources/login.js
@@ -3,7 +3,6 @@ async function hashPassword(password) {
   const data = encoder.encode(password);
   const hashBuffer = await crypto.subtle.digest('SHA-256', data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  // Convert bytes to hex string
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 }
 
