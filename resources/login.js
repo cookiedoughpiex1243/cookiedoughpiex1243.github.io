e91a940ade87f60e68f452cd8b516eee70c389d4
cookiedoughpiex1243 <joshxgaming1243@gmail.com>
2026-02-10 19:25:45 +0530

    Update login.js

--- resources/login.js
+++ resources/login.js
@@ -2,8 +2,8 @@ document.getElementById('loginbtn').addEventListener('click', function () {
   const user = document.getElementById('username');
   const pass = document.getElementById('password');
   const message = document.getElementById('message');
-  if (user == "josh" && pass == "347379") {
-    message.style.color=#39ff14;
+  if (user.value == "josh" && pass.value == "347379") {
+    message.style.color= "#39ff14";
     message.innerText = " Access Granted, redirecting..";
     setTimeout (() => {
       window.location.href = "https://joyfoodsunshine.com"}, 1000);
