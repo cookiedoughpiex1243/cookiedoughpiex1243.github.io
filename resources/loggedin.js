3205a8ea48e3ce0635e05da3c8a1f5763d22a36e
cookiedoughpiex1243 <joshxgaming1243@gmail.com>
2026-02-12 20:07:09 +0530

    Update loggedin.js

--- resources/loggedin.js
+++ resources/loggedin.js
@@ -1,10 +1,9 @@
 if (sessionStorage.getItem('loggedIn') === 'true') {
 }
 else {
-  window.location.href = "login");
-                       }
+  window.location.href = "login";
+}
                        
-
 window.onload = function () {
   sessionStorage.removeItem('loggedIn');
 }
