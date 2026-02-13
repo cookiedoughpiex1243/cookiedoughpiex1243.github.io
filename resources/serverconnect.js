c08785e3f841d3ce1a23beb52f2ff8395dcfd42a
cookiedoughpiex1243 <joshxgaming1243@gmail.com>
2026-02-13 18:13:24 +0530

    Create serverconnect.js

---
+++ resources/serverconnect.js
@@ -0,0 +1,20 @@
+const CLOUD_URL = "https://josh-backend-om8q.onrender.com";
+async function getFromServer() {
+  try {
+    const response = await fetch(`${CLOUD_URL}/load`);
+        const data = await response.json();
+        document.getElementById('userNote').value = data.message || "";
+    } catch (err) {
+        console.error("Server is still waking up...", err);
+    }
+}
+
+async function saveToServer() {
+    const text = document.getElementById('userMsg').value;
+    await fetch(`${CLOUD_URL}/save`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: text })
+    });
+    alert("Saved to server");
+}
